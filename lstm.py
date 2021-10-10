9a56caf94268b1b68e8b2b796340026f444316d2
masa-ymd <masa-ymd@nifty.com>
2021-10-10 16:05:02 +0000

    x

--- lstm.py
+++ lstm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import logging
 
 import tensorflow as tf, gc
 from tensorflow import keras
@@ -23,58 +24,22 @@ train = pd.read_csv(f"{DATA_DIR}/preprocessed_train.csv")
 
 #train = pd.read_csv('/root/kaggle/ventilator-pressure-prediction/data/train.csv')
 
-def add_features(df):
-    df['area'] = df['time_step'] * df['u_in']
-    df['area'] = df.groupby('breath_id')['area'].cumsum()
-    
-    df['u_in_cumsum'] = (df['u_in']).groupby(df['breath_id']).cumsum()
-    
-    df['u_in_lag1'] = df.groupby('breath_id')['u_in'].shift(1)
-    df['u_out_lag1'] = df.groupby('breath_id')['u_out'].shift(1)
-    df['u_in_lag_back1'] = df.groupby('breath_id')['u_in'].shift(-1)
-    df['u_out_lag_back1'] = df.groupby('breath_id')['u_out'].shift(-1)
-    df['u_in_lag2'] = df.groupby('breath_id')['u_in'].shift(2)
-    df['u_out_lag2'] = df.groupby('breath_id')['u_out'].shift(2)
-    df['u_in_lag_back2'] = df.groupby('breath_id')['u_in'].shift(-2)
-    df['u_out_lag_back2'] = df.groupby('breath_id')['u_out'].shift(-2)
-    df['u_in_lag3'] = df.groupby('breath_id')['u_in'].shift(3)
-    df['u_out_lag3'] = df.groupby('breath_id')['u_out'].shift(3)
-    df['u_in_lag_back3'] = df.groupby('breath_id')['u_in'].shift(-3)
-    df['u_out_lag_back3'] = df.groupby('breath_id')['u_out'].shift(-3)
-    df['u_in_lag4'] = df.groupby('breath_id')['u_in'].shift(4)
-    df['u_out_lag4'] = df.groupby('breath_id')['u_out'].shift(4)
-    df['u_in_lag_back4'] = df.groupby('breath_id')['u_in'].shift(-4)
-    df['u_out_lag_back4'] = df.groupby('breath_id')['u_out'].shift(-4)
-    df = df.fillna(0)
-    
-    df['breath_id__u_in__max'] = df.groupby(['breath_id'])['u_in'].transform('max')
-    df['breath_id__u_out__max'] = df.groupby(['breath_id'])['u_out'].transform('max')
-    
-    df['u_in_diff1'] = df['u_in'] - df['u_in_lag1']
-    df['u_out_diff1'] = df['u_out'] - df['u_out_lag1']
-    df['u_in_diff2'] = df['u_in'] - df['u_in_lag2']
-    df['u_out_diff2'] = df['u_out'] - df['u_out_lag2']
-    
-    df['breath_id__u_in__diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
-    df['breath_id__u_in__diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
-    
-    df['breath_id__u_in__diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
-    df['breath_id__u_in__diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
-    
-    df['u_in_diff3'] = df['u_in'] - df['u_in_lag3']
-    df['u_out_diff3'] = df['u_out'] - df['u_out_lag3']
-    df['u_in_diff4'] = df['u_in'] - df['u_in_lag4']
-    df['u_out_diff4'] = df['u_out'] - df['u_out_lag4']
-    df['cross']= df['u_in']*df['u_out']
-    df['cross2']= df['time_step']*df['u_out']
-    
-    df['R'] = df['R'].astype(str)
-    df['C'] = df['C'].astype(str)
-    df['R__C'] = df["R"].astype(str) + '__' + df["C"].astype(str)
-    df = pd.get_dummies(df)
-    return df
-
-#train = add_features(train)
+# Detect hardware, return appropriate distribution strategy
+print(tf.version.VERSION)
+tf.get_logger().setLevel(logging.ERROR)
+try: # detect TPU
+    tpu = None
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except ValueError: # detect GPU(s) and enable mixed precision
+    strategy = tf.distribute.MirroredStrategy() # works on GPU and multi-GPU
+    policy = tf.keras.mixed_precision.experimental.Policy('mixed_float16')
+    tf.config.optimizer.set_jit(True) # XLA compilation
+    tf.keras.mixed_precision.experimental.set_policy(policy)
+    print('Mixed precision enabled')
+print("REPLICAS: ", strategy.num_replicas_in_sync)
 
 targets = train[['pressure']].to_numpy().reshape(-1, 80)
 train.drop(['pressure', 'id', 'breath_id'], axis=1, inplace=True)
@@ -96,7 +61,8 @@ NUM_FOLDS = 10
 
 gpu_strategy = tf.distribute.get_strategy()
 
-with gpu_strategy.scope():
+with strategy.scope():
+#with gpu_strategy.scope():
     kf = KFold(n_splits=NUM_FOLDS, shuffle=True, random_state=2021)
     test_preds = []
     for fold, (train_idx, test_idx) in enumerate(kf.split(train, targets)):
