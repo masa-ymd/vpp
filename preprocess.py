07fbfb865321bce2abfd36e7cb3feb429d574ff9
masa-ymd <masa.ymd.0703@gmail.com>
2021-10-17 02:27:13 +0900

    x

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,7 @@ def add_features(df):
     #df['area'] = df['delta_time'] * df['u_in']
     df['area'] = df.groupby('breath_id')['area'].cumsum()
 
-    print("1")
+    print("Step-1...Completed")
 
     # rewritten calculation of lag features from this notebook: https://www.kaggle.com/patrick0302/add-lag-u-in-as-new-feat
     # some of ideas from this notebook: https://www.kaggle.com/mst8823/google-brain-lightgbm-baseline
@@ -37,7 +37,7 @@ def add_features(df):
     df['u_out_lag_back4'] = df.groupby('breath_id')['u_out'].shift(-4, fill_value=0)
     df = df.fillna(0)
 
-    print("2")
+    print("Step-2...Completed")
 
     # max value of u_in and u_out for each breath
     df['u_in_max'] = df.groupby(['breath_id'])['u_in'].transform('max')
@@ -49,7 +49,7 @@ def add_features(df):
     #df['u_out_max'] = df.groupby(['breath_id'])['u_out'].transform('max')
     #df['u_out_min'] = df.groupby(['breath_id'])['u_out'].transform('min')
 
-    print("3")
+    print("Step-3...Completed")
 
     # difference between consequitive values
     df['u_in_diff1'] = df['u_in'] - df['u_in_lag1']
@@ -83,22 +83,26 @@ def add_features(df):
     #df.loc[df['time_step'] == 0, 'u_in_diff_back4'] = 0
     #df.loc[df['time_step'] == 0, 'u_out_diff4'] = 0
 
-    print("4")
+    print("Step-4...Completed")
 
     df['area_abs'] = df['u_in_diff_back1'] * df['delta_time']
     df['uin_in_time'] = df['u_in_diff_back1'] / df['delta_time']
+
+    print("Step-5...Completed")
 
     df['one'] = 1
     df['count'] = (df['one']).groupby(df['breath_id']).cumsum()
     df['u_in_cummean'] =df['u_in_cumsum'] / df['count']
 
-    print("5")
+    print("Step-6...Completed")
 
     # difference between the current value of u_in/u_out and the max value within the breath
     df['u_in_diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
     df['u_in_diffmin'] = df.groupby(['breath_id'])['u_in'].transform('min') - df['u_in']
     df['u_in_diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
     df['u_in_diffmean'] = df.groupby(['breath_id'])['u_in'].transform('median') - df['u_in']
+
+    print("Step-7...Completed")
 
     df['breath_id_lag']=df['breath_id'].shift(1).fillna(0)
     df['breath_id_lag2']=df['breath_id'].shift(2).fillna(0)
@@ -109,13 +113,15 @@ def add_features(df):
     df['breath_id__u_in_lag2'] = df['u_in'].shift(2).fillna(0)
     df['breath_id__u_in_lag2'] = df['breath_id__u_in_lag2'] * df['breath_id_lag2same']
 
-    print("6")
+    print("Step-8...Completed")
 
     # OHE
     df['R__C'] = df["R"].astype(str) + '__' + df["C"].astype(str)
     df = df.merge(pd.get_dummies(df['R'], prefix='R'), left_index=True, right_index=True).drop(['R'], axis=1)
     df = df.merge(pd.get_dummies(df['C'], prefix='C'), left_index=True, right_index=True).drop(['C'], axis=1)
     df = df.merge(pd.get_dummies(df['R__C'], prefix='R__C'), left_index=True, right_index=True).drop(['R__C'], axis=1)
+
+    print("Step-9...Completed")
 
     df['time_step_diff'] = df.groupby('breath_id')['time_step'].diff().fillna(0)
     df['ewm_u_in_mean'] = (df\
@@ -132,18 +138,18 @@ def add_features(df):
                                                                     "15_in_mean":"mean"})\
                                                                .reset_index(level=0,drop=True))
 
-    print("7")
+    print("Step-10...Completed")
 
     # https://www.kaggle.com/c/ventilator-pressure-prediction/discussion/273974
     df['u_in_cumsum'] = df.groupby(['breath_id'])['u_in'].cumsum()
     df['time_step_cumsum'] = df.groupby(['breath_id'])['time_step'].cumsum()
 
-    print("8")
+    print("Step-11...Completed")
 
     df['cross'] = df['u_in'] * df['u_out']
     df['cross2'] = df['time_step'] * df['u_out']
 
-    print("9")
+    print("Step-12...Completed")
 
     return df
 
