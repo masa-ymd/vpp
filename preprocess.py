29075668ff9061d5fc284440ef9e5b51ce424e9b
masa-ymd <masa.ymd.0703@gmail.com>
2021-10-17 01:47:06 +0900

    x

--- preprocess.py
+++ preprocess.py
@@ -9,10 +9,10 @@ df_train = pd.read_csv(f"{DATA_DIR}/train.csv")
 
 def add_features(df): 
 
-    #df['area'] = df['time_step'] * df['u_in']
+    df['area'] = df['time_step'] * df['u_in']
     df['delta_time'] = df['time_step'].shift(-1, fill_value=0) - df['time_step']
-    df['area'] = df['delta_time'] * df['u_in']
-    #df['area'] = df.groupby('breath_id')['area'].cumsum()
+    #df['area'] = df['delta_time'] * df['u_in']
+    df['area'] = df.groupby('breath_id')['area'].cumsum()
 
     print("1")
 
@@ -35,15 +35,19 @@ def add_features(df):
     df['u_out_lag4'] = df.groupby('breath_id')['u_out'].shift(4, fill_value=0)
     df['u_in_lag_back4'] = df.groupby('breath_id')['u_in'].shift(-4, fill_value=0)
     df['u_out_lag_back4'] = df.groupby('breath_id')['u_out'].shift(-4, fill_value=0)
-    #df = df.fillna(0)
+    df = df.fillna(0)
 
     print("2")
 
     # max value of u_in and u_out for each breath
     df['u_in_max'] = df.groupby(['breath_id'])['u_in'].transform('max')
-    df['u_out_max'] = df.groupby(['breath_id'])['u_out'].transform('max')
+    df['u_in_mean'] = df.groupby(['breath_id'])['u_in'].transform('mean')
     df['u_in_min'] = df.groupby(['breath_id'])['u_in'].transform('min')
-    df['u_out_min'] = df.groupby(['breath_id'])['u_out'].transform('min')
+    df['u_in_diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
+    df['u_in_diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
+    df['u_in_diffmin'] = df.groupby(['breath_id'])['u_in'].transform('min') - df['u_in']
+    #df['u_out_max'] = df.groupby(['breath_id'])['u_out'].transform('max')
+    #df['u_out_min'] = df.groupby(['breath_id'])['u_out'].transform('min')
 
     print("3")
 
@@ -51,33 +55,42 @@ def add_features(df):
     df['u_in_diff1'] = df['u_in'] - df['u_in_lag1']
     df['u_in_diff_back1'] = df['u_in_lag_back1'] - df['u_in']
     df['u_out_diff1'] = df['u_out'] - df['u_out_lag1']
+    df['u_out_diff_back1'] = df['u_out_lag_back1'] - df['u_out']
     df['u_in_diff2'] = df['u_in'] - df['u_in_lag2']
     df['u_in_diff_back2'] = df['u_in_lag_back2'] - df['u_in']
     df['u_out_diff2'] = df['u_out'] - df['u_out_lag2']
+    df['u_out_diff_back2'] = df['u_out_lag_back2'] - df['u_out']
     df['u_in_diff3'] = df['u_in'] - df['u_in_lag3']
     df['u_in_diff_back3'] = df['u_in_lag_back3'] - df['u_in']
     df['u_out_diff3'] = df['u_out'] - df['u_out_lag3']
+    df['u_out_diff_back3'] = df['u_out_lag_back3'] - df['u_out']
     df['u_in_diff4'] = df['u_in'] - df['u_in_lag4']
     df['u_in_diff_back4'] = df['u_in_lag_back4'] - df['u_in']
     df['u_out_diff4'] = df['u_out'] - df['u_out_lag4']
+    df['u_out_diff_back4'] = df['u_out_lag_back4'] - df['u_out']
     # from here: https://www.kaggle.com/yasufuminakama/ventilator-pressure-lstm-starter
-    df.loc[df['time_step'] == 0, 'u_in_diff1'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff_back1'] = 0
-    df.loc[df['time_step'] == 0, 'u_out_diff1'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff2'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff_back2'] = 0
-    df.loc[df['time_step'] == 0, 'u_out_diff2'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff3'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff_back3'] = 0
-    df.loc[df['time_step'] == 0, 'u_out_diff3'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff4'] = 0
-    df.loc[df['time_step'] == 0, 'u_in_diff_back4'] = 0
-    df.loc[df['time_step'] == 0, 'u_out_diff4'] = 0
+    df.fillna(0)
+    #df.loc[df['time_step'] == 0, 'u_in_diff1'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff_back1'] = 0
+    #df.loc[df['time_step'] == 0, 'u_out_diff1'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff2'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff_back2'] = 0
+    #df.loc[df['time_step'] == 0, 'u_out_diff2'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff3'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff_back3'] = 0
+    #df.loc[df['time_step'] == 0, 'u_out_diff3'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff4'] = 0
+    #df.loc[df['time_step'] == 0, 'u_in_diff_back4'] = 0
+    #df.loc[df['time_step'] == 0, 'u_out_diff4'] = 0
 
     print("4")
 
     df['area_abs'] = df['u_in_diff_back1'] * df['delta_time']
     df['uin_in_time'] = df['u_in_diff_back1'] / df['delta_time']
+
+    df['one'] = 1
+    df['count'] = (df['one']).groupby(df['breath_id']).cumsum()
+    df['u_in_cummean'] =df['u_in_cumsum'] / df['count']
 
     print("5")
 
@@ -87,6 +100,15 @@ def add_features(df):
     df['u_in_diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
     df['u_in_diffmean'] = df.groupby(['breath_id'])['u_in'].transform('median') - df['u_in']
 
+    df['breath_id_lag']=df['breath_id'].shift(1).fillna(0)
+    df['breath_id_lag2']=df['breath_id'].shift(2).fillna(0)
+    df['breath_id_lagsame']=np.select([df['breath_id_lag']==df['breath_id']],[1],0)
+    df['breath_id_lag2same']=np.select([df['breath_id_lag2']==df['breath_id']],[1],0)
+    df['breath_id__u_in_lag'] = df['u_in'].shift(1).fillna(0)
+    df['breath_id__u_in_lag'] = df['breath_id__u_in_lag'] * df['breath_id_lagsame']
+    df['breath_id__u_in_lag2'] = df['u_in'].shift(2).fillna(0)
+    df['breath_id__u_in_lag2'] = df['breath_id__u_in_lag2'] * df['breath_id_lag2same']
+
     print("6")
 
     # OHE
@@ -94,6 +116,21 @@ def add_features(df):
     df = df.merge(pd.get_dummies(df['R'], prefix='R'), left_index=True, right_index=True).drop(['R'], axis=1)
     df = df.merge(pd.get_dummies(df['C'], prefix='C'), left_index=True, right_index=True).drop(['C'], axis=1)
     df = df.merge(pd.get_dummies(df['R__C'], prefix='R__C'), left_index=True, right_index=True).drop(['R__C'], axis=1)
+
+    df['time_step_diff'] = df.groupby('breath_id')['time_step'].diff().fillna(0)
+    df['ewm_u_in_mean'] = (df\
+                           .groupby('breath_id')['u_in']\
+                           .ewm(halflife=9)\
+                           .mean()\
+                           .reset_index(level=0,drop=True))
+    df[["15_in_sum","15_in_min","15_in_max","15_in_mean"]] = (df\
+                                                              .groupby('breath_id')['u_in']\
+                                                              .rolling(window=15,min_periods=1)\
+                                                              .agg({"15_in_sum":"sum",
+                                                                    "15_in_min":"min",
+                                                                    "15_in_max":"max",
+                                                                    "15_in_mean":"mean"})\
+                                                               .reset_index(level=0,drop=True))
 
     print("7")
 
